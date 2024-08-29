ffbc8411a99da2e9d8fe492293e84cbe5818e744
Dev Mittal <154676624+devmittal420@users.noreply.github.com>
2024-08-30 01:31:11 +0530

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,6 @@ import InputForm from './Day-4/.form-Project/1.project';
 
 createRoot(document.getElementById('root')).render(
   <>  
-  {/* <About/>
-  <SampleGreeting/> */}
-  {/* <UNameExample />
-  <Counter /> */}
-  {/* <ToggleExample /> */}
-  {/* <BegginerForm />  */}
-  {/* <MidLevelEngineerForm /> */}
   <InputForm />
   </>
 )
